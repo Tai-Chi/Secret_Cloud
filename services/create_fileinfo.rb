b0301c0e2b5d9606aac7f7a47a10e508c024478c
alan23273850 <alan23273850@gmail.com>
2017-05-06 17:21:52 +0800

    fix bug: forgot to add size to createfileinfo service

--- services/create_fileinfo.rb
+++ services/create_fileinfo.rb
@@ -1,6 +1,6 @@
 class CreateFileinfo
   def self.call(fileattr)
-    fileinfo = Fileinfo.new(name_secure: fileattr[:name], parent_id: fileattr[:parent_id], account_id: fileattr[:account_id], portion: fileattr[:portion], gaccount_id: fileattr[:gaccount_id], gfid_secure: fileattr[:gfid])
+    fileinfo = Fileinfo.new(name_secure: fileattr[:name], parent_id: fileattr[:parent_id], account_id: fileattr[:account_id], portion: fileattr[:portion], gaccount_id: fileattr[:gaccount_id], gfid_secure: fileattr[:gfid], size: fileattr[:size])
     fileinfo.name = fileattr[:name]
     fileinfo.gfid = fileattr[:gfid]
     fileinfo.save
