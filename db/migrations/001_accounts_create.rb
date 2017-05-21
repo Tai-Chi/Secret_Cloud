3d5cb3215ddbd34fcea78e7cd09f3c31d987efba
jpdyuki <joypad.y.t.h@gmail.com>
2017-05-22 03:43:54 +0800

    add 'email' field into 'account' table

--- db/migrations/001_accounts_create.rb
+++ db/migrations/001_accounts_create.rb
@@ -5,6 +5,7 @@ Sequel.migration do
     create_table(:accounts) do
       primary_key :id
       String :name, null: false
+      String :email, null: false
       String :passwd_hash, null: false, text: true
       String :salt, null: false
     end
--- services/create_account.rb
+++ services/create_account.rb
@@ -1,6 +1,6 @@
 class CreateAccount
   def self.call(registration)
-    account = Account.new(name: registration[:name], passwd: registration[:passwd])
+    account = Account.new(name: registration[:name], email: registration[:email], passwd: registration[:passwd])
     account.passwd = registration[:passwd]
     account.save
   end
