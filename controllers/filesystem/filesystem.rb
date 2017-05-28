ca2763bcdc152f972105d38293eca87d2c8c49cb
alan23273850 <alan23273850@gmail.com>
2017-05-29 00:45:54 +0800

    add filesystem api to support file list on website

---
+++ controllers/filesystem/filesystem.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+
+# Class for route /filesystem
+class FileSystemSyncAPI < Sinatra::Base
+  # For each folder, generate a corresponding
+  # hash structure for it.
+  def DFS(folder)
+    return nil if folder == nil
+    filesystem = Hash.new
+    if folder.portion == 0
+      folder.list.each do |file|
+        filesystem[file.name] = DFS(file)
+      end
+      return filesystem
+    else
+      return folder.name
+      # For now we still assume each file has only 1 portion.
+    end
+  end
+
+  post '/filesystem/?' do
+    content_type 'application/json'
+    begin
+      username = JsonParser.call(request, 'username')
+      halt 403, 'Username cannot be null!!' if username == nil
+      username = username.to_s
+      tree = self.get_tree(username)
+      halt 403, 'This account does not exist!!' if tree == nil
+      body self.DFS(tree.root_dir).to_json
+      logger.info "RETURN FILESYSTEM SUCCESSFULLY"
+      status 200
+    rescue => e
+      logger.info "FAILED to return the filesystem: #{e.inspect}"
+      status 400
+    end
+  end
+end
