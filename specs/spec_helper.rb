f4587b37d87eb54a0f58527dea6e82007916f055
jpdyuki <joypad.y.t.h@gmail.com>
2017-06-08 17:39:19 +0800

    modify specs due to auth_token

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -10,3 +10,12 @@ include Rack::Test::Methods
 def app
   FileSystemSyncAPI
 end
+
+def get_authorized_client(username, passwd)
+  browser = Rack::Test::Session.new(Rack::MockSession.new(FileSystemSyncAPI))
+  auth = AuthenticateAccount.call(username: username,
+                                  passwd: passwd)
+  browser.header 'AUTHORIZATION', "Bearer #{auth[:auth_token]}"
+  browser.header 'CONTENT_TYPE', 'application/json'
+  browser
+end
